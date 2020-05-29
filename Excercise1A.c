63ff8c92abc3d109f3f2df71c383650294121cb8
Neguib Metri <61012192+Neguib-Metri@users.noreply.github.com>
2020-05-28 23:18:55 -0500

    A: Code Programs in C that do the following...

---
+++ Excercise1A.c
@@ -0,0 +1,29 @@
+//1. Write a program to input any character and check whether it is alphabet, digit or special character. //
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ng; //variable//
+	//getting char from user//
+    printf("Enter a single character:\n");
+    scanf("%c", &ng);
+
+	//alphabeth check(capital or lowercase)
+    if((ng>='a'&&ng<='z') || (ng>='A'&&ng<='Z'))
+    {
+        printf("'%c' is a letter!", ng);
+    }
+    else if(ng >= '0' && ng <= '9')//checking if char is a number
+    {
+        printf("'%c' is a digit!", ng);
+    }
+    else //if it is not a letter or number, print "special character"
+    {
+        printf("'%c' is a special character!", ng);
+    }
+
+    return 0;
+}
---
+++ Excercise2A.c
@@ -0,0 +1,16 @@
+// 2. Write a program to check whether a number is even or odd. //
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+int number;//variable declaration
+printf("enter a number:\n");
+scanf("%d", &number);//getting the number
+if(number%2==0)//condition for it to be even
+printf("Thats an even number!");//print for even case
+else
+printf("Thats an odd number!");//print for odd case
+return 0;
+}
